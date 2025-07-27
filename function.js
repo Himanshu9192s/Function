461d2f82e8fa2fc385b648f04b3aa4c5b316bcd3
Himanshu9192s <officialhimanshusen@gmail.com>
2025-07-27 12:01:22 +0530

    add to github

---
+++ function.js
@@ -0,0 +1,109 @@
+// function myname() {
+
+//     const name = 'himanshu sen'
+//     return name,console.log(name);
+    
+
+// }
+
+// console.log(myname());
+// myname()
+
+//=====================================================================================================================
+
+// function myname() {
+//     const name = 'himanshu sen';
+//     console.log(name);  // This logs inside the function
+//     return name;        // This makes the function return the name
+// }
+
+// console.log(myname()); // This logs the returned value
+// myname()
+
+
+//=====================================================================================================================
+
+// function addnum(num1,num2){
+//     console.log(Number(num1)+Number(num2))
+// }
+
+// addnum(5,6)
+// addnum(5,'6')
+// addnum('5',6)
+// addnum('5','6')
+// addnum(5,'a')
+// addnum('5','a')
+
+
+// =====================================================================================================================
+
+
+// function para(a,b){
+
+//     console.log(`addition is ${Number(a)+Number(b)}`);
+//     console.log(`subtraction is ${Number(a)-Number(b)}`);
+//     console.log(`multiply is ${Number(a)*Number(b)}`);
+//     console.log(`divide is ${Number(a)/Number(b)}`);
+    
+// }
+
+// para(6,5)
+// console.log("=======================================");
+// para(6,null)
+// console.log("=======================================");
+// para(6,undefined)
+
+//=====================================================================================================================
+
+
+// function para(a,b){
+
+//     // console.log(`addition is ${Number(a)+Number(b)}`);
+//     // console.log(`subtraction is ${Number(a)-Number(b)}`);
+//     // console.log(`multiply is ${Number(a)*Number(b)}`);
+//     // console.log(`divide is ${Number(a)/Number(b)}`);
+    
+//     let result = a + b 
+//     return result
+// }
+
+// const result = para(4,5)
+// para(4,5)
+
+// console.log(result)
+
+// =====================================================================================================================
+
+
+// function para(a, b) {
+//     let result = a + b;
+//     console.log("Inside function:", result);
+//     return result;
+// }
+
+// const result = para(4, 5);  // result = 9
+// console.log("Outside function:", result);
+
+// console.log(para(1,2))
+
+
+// =====================================================================================================================
+
+
+// function addnum(num1,num2){
+
+//     return num1+num2;
+
+// }
+
+// addnum(4,5)
+
+// const result = addnum(4,5)
+
+// console.log("result : " , result )
+
+// console.log("result : " , addnum(4,5) )
+
+
+// =====================================================================================================================
+
